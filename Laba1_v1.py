b380f599f9696fbe5eb89363540d040c92628187
dashkors <40039045+dashkors@users.noreply.github.com>
2018-06-08 01:43:49 +0700

    Initial commit

---
+++ Laba1_v1.py
@@ -0,0 +1,37 @@
+
+
+class Animals:
+
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+        if self.type == "beast":
+            self.cell = "_aviary for beasts_"
+        elif self.type == "fish":
+            self.cell = "_aquarium_"
+        elif self.type <> "bird":
+            self.cell = "_cell_"
+        else:
+            self.cell = "_cage for birds_"
+
+    def printinfo(self):
+        print("Питомец:" , self.name , "-" , self.type, "Живет в ", self.cell)
+
+
+class Iterator:
+    def iterateAni(self):
+        listname = ["Аист", "Белый слон", "Шимпанзе", "Лев", "Золотая рыбка"]
+        listtype = ["bird" , "beast" , "beast", "beast", "fish"]
+        for i in range(0, 4):
+            ani = Animals(listname[i] , listtype[i])
+            ani.printinfo()
+
+
+def main():
+    iterator = Iterator()
+    iterator.iterateAni()
+
+
+print("Животные в зоопарке:")
+if __name__ == "__main__":
+    main()
