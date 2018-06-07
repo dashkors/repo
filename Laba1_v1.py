1bb29adf65843a0c3c5986bc7a04e7ea5713f0b2
dashkors <40039045+dashkors@users.noreply.github.com>
2018-06-08 02:30:46 +0700

    change
    исправление ошибок flake8

--- Laba1_v1.py
+++ Laba1_v1.py
@@ -9,19 +9,19 @@ class Animals:
             self.cell = "_aviary for beasts_"
         elif self.type == "fish":
             self.cell = "_aquarium_"
-        elif self.type <> "bird":
+        elif self.type != "bird":
             self.cell = "_cell_"
         else:
             self.cell = "_cage for birds_"
 
     def printinfo(self):
-        print("Питомец:" , self.name , "-" , self.type, "Живет в ", self.cell)
+        print("Питомец:", self.name, "-", self.type, "Живет в ", self.cell)
 
 
 class Iterator:
     def iterateAni(self):
         listname = ["Аист", "Белый слон", "Шимпанзе", "Лев", "Золотая рыбка"]
-        listtype = ["bird" , "beast" , "beast", "beast", "fish"]
+        listtype = ["bird", "beast", "beast", "beast", "fish"]
         for i in range(0, 4):
             ani = Animals(listname[i] , listtype[i])
             ani.printinfo()
